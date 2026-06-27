200c96c8b0dd45751b99f371c79ce012b010e7a3
Noorullah Zamindar <151749928+NoorullahZamindar-007@users.noreply.github.com>
2026-06-27 20:16:06 +0430

    Reformat config.py for consistency

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ QUALITY_FORMATS = {
     "1080p": "bestvideo[height<=1080]+bestaudio/best[height<=1080]/best",
     "720p": "bestvideo[height<=720]+bestaudio/best[height<=720]/best",
     "480p": "bestvideo[height<=480]+bestaudio/best[height<=480]/best",
-    "Audio only MP3": "bestaudio/best",
+    "Audio only MP3": "bestaudio/best", 
 }
 
 WINDOW_SIZE = "1040x760"
